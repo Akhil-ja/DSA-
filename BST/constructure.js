1cf9dc1daebcb2e88bcfc08b34f3cb68e1b22562
Akhil Jagadish <akhiljagadish124@gmail.com>
2024-05-28 18:54:59 +0530

    BTS constructor

---
+++ BST/constructure.js
@@ -0,0 +1,18 @@
+class Node{
+    constructure(value){
+        this.value=value
+        this.left=null
+        this.right=null 
+    }
+}
+
+
+class BTS {
+    constructure(){
+       this.root=null
+    }
+}
+
+let myTree=new BTS()
+
+myTree
